8faf3377e5c31ee13ec10139b37eb63c5fcd4325
dharm <dharmjat45@gmail.com>
2022-01-04 01:51:18 +0530

    Modals Created

---
+++ Modals/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+
+const overlay = document.querySelector(".overlay");
+
+const btnCloseModal = document.querySelector(".close-modal");
+
+const btnsOpenModel = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  console.log("button Clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < btnsOpenModel.length; i++)
+  btnsOpenModel[i].addEventListener("click", openModal);
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
