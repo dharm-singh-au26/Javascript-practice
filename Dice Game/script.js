9b20c0721ea685ea89d56324a4e70582bcd99dee
dharm <dharmjat45@gmail.com>
2022-01-04 21:07:19 +0530

    new game button functionality added

--- Dice Game/script.js
+++ Dice Game/script.js
@@ -12,39 +12,85 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
+let scores, currentScore, activePlayer, playing;
 // Starting Condition
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+// score0El.textContent = 0;
+// score1El.textContent = 0;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add("hidden");
+
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+init();
+
+const switchplayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
 // Rolling dice functionality
 
 btnRoll.addEventListener("click", function () {
-  // 1 Genrating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    // 1 Genrating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // 2 display dice
-  diceEl.classList.remove("hidden");
-  diceEl.src = `dice-${dice}.png`;
+    // 2 display dice
+    diceEl.classList.add("hidden");
+    diceEl.src = `dice-${dice}.png`;
 
-  // .  check for rolled 1: if true ,switch to next Player
+    // .  check for rolled 1: if true ,switch to next Player
 
-  if (dice !== 1) {
-    // Add dice to current score
-    currentScore += dice;
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
 
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Switch to next Player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle("player--active");
-    player1El.classList.toggle("player--active");
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next Player
+      switchplayer();
+    }
   }
 });
+
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    // 1. Add current score to  active players score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. Check If plyers score is >= 100
+    if (scores[activePlayer] >= 20) {
+      // Finish the Game
+      playing = false;
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--active");
+    } else {
+      // switch to next player
+      switchplayer();
+    }
+  }
+});
+
+btnNew.addEventListener("click", init);
