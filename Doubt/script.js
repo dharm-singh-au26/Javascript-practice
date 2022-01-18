1f697602691fb50d0a04ae8fce550d21c841c58c
dharm <dharmjat45@gmail.com>
2022-01-18 13:03:34 +0530

    function inside function doubt

---
+++ Doubt/script.js
@@ -0,0 +1,14 @@
+/* 1 How it will work
+"use strict";
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet("Hey");
+greeterHey("Dharm");
+greeterHey("Ram");
+
+*/
