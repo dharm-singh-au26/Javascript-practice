73ad6d41b11185b2c0e9dc4f190296758649fbc9
dharm <dharmjat45@gmail.com>
2022-01-19 23:05:43 +0530

    using while loop

--- Practice/Tabel/script.js
+++ Practice/Tabel/script.js
@@ -1,5 +1,14 @@
 const number = prompt("Please enter number of Tabel");
-
+// using for loop
 for (let i = 1; i <= 10; i++) {
   console.log(`${number} * ${i} = ${number * i}`);
+}
+
+// using while loop
+
+let i = 1;
+while (i <= 10) {
+  console.log(`${number} * ${i} = ${number * i}`);
+
+  i++;
 }
