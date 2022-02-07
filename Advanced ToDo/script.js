88d61029e4e12b1dc3a7f2bed756412e084addb2
dharm <dharmjat45@gmail.com>
2022-02-07 18:12:26 +0530

    toto app complete without css

---
+++ Advanced ToDo/script.js
@@ -0,0 +1,121 @@
+// console.log("Helo");
+showtask();
+let addtaskinput = document.getElementById("addtaskinput");
+let addtaskbtn = document.getElementById("addtaskbtn");
+
+addtaskbtn.addEventListener("click", function () {
+  addtaskinputval = addtaskinput.value;
+  if (addtaskinputval.trim() != 0) {
+    let webtask = localStorage.getItem("localtask");
+    if (webtask === null) {
+      taskObj = [];
+    } else {
+      taskObj = JSON.parse(webtask);
+    }
+    taskObj.push(addtaskinputval);
+    localStorage.setItem("localtask", JSON.stringify(taskObj));
+
+    addtaskinput.value = "";
+  }
+  showtask();
+});
+
+function showtask() {
+  let webtask = localStorage.getItem("localtask");
+  if (webtask === null) {
+    taskObj = [];
+  } else {
+    taskObj = JSON.parse(webtask);
+  }
+
+  let html = "";
+  let addedtasklist = document.getElementById("addedtasklist");
+
+  taskObj.forEach((item, index) => {
+    html += `<tr>
+      <th>${index + 1}</th>
+      <td>${item}</td>
+      <td><button type="button" onclick="edittask(${index})">Edit</button></td>
+      <td><button type="button" onclick="deleteitem(${index})">Delete</button></td>
+    </tr> `;
+  });
+  addedtasklist.innerHTML = html;
+}
+
+function edittask(index) {
+  let saveindex = document.getElementById("saveindex");
+  let addtaskbtn = document.getElementById("addtaskbtn");
+  let savetaskbtn = document.getElementById("savetaskbtn");
+  saveindex.value = index;
+  let webtask = localStorage.getItem("localtask");
+  let taskObj = JSON.parse(webtask);
+  addtaskinput.value = taskObj[index];
+  addtaskbtn.style.display = "none";
+  savetaskbtn.style.display = "block";
+}
+
+// save task
+let savetaskbtn = document.getElementById("savetaskbtn");
+savetaskbtn.addEventListener("click", function () {
+  let webtask = localStorage.getItem("localtask");
+  let taskObj = JSON.parse(webtask);
+  let saveindex = document.getElementById("saveindex").value;
+  taskObj[saveindex] = addtaskinput.value;
+  savetaskbtn.style.display = "none";
+  addtaskbtn.style.display = "block";
+
+  localStorage.setItem("localtask", JSON.stringify(taskObj));
+  addtaskinput.value = "";
+  showtask();
+});
+
+// delete itam
+function deleteitem(index) {
+  let webtask = localStorage.getItem("localtask");
+  let taskObj = JSON.parse(webtask);
+  taskObj.splice(index, 1);
+  localStorage.setItem("localtask", JSON.stringify(taskObj));
+
+  showtask();
+}
+
+// delete all
+
+let deleteallbtn = document.getElementById("deleteallbtn");
+
+deleteallbtn.addEventListener("click", function () {
+  let savetaskbtn = document.getElementById("savetaskbtn");
+  let addtaskbtn = document.getElementById("addtaskbtn");
+  let addtaskinput = document.getElementById("addtaskinput");
+
+  let webtask = localStorage.getItem("localtask");
+  let taskObj = JSON.parse(webtask);
+  if (webtask === null) {
+    taskObj = [];
+  } else {
+    taskObj = JSON.parse(webtask);
+    taskObj = [];
+  }
+  savetaskbtn.style.display = "none";
+  addtaskbtn.style.display = "block";
+  addtaskinput.value = "";
+  localStorage.setItem("localtask", JSON.stringify(taskObj));
+  showtask();
+});
+
+// search list
+
+let searchtextbox = document.getElementById("searchtextbox");
+searchtextbox.addEventListener("input", function () {
+  let trList = document.querySelectorAll("tr");
+  Array.from(trList).forEach(function (item) {
+    let searchedText = item.getElementsByTagName("td")[0].innerText;
+    let searchtextboxval = searchtextbox.value;
+    let re = new RegExp(searchtextboxval, "gi");
+    if (searchedText.match(re)) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+});
