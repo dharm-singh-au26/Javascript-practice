55f64a74c25d5d2b20eeddc836e36fc575ab1373
Dharm Singh <dharmjat45@gmail.com>
2022-07-24 15:39:47 +0530

    sum calculator

---
+++ sum calculator/app.js
@@ -0,0 +1,13 @@
+
+
+
+const  sum  = () => {
+    let a = document.getElementById("firstnumber").value
+    let b = document.getElementById("secondnumber").value
+    let sum = Number(a) + Number(b)
+    document.getElementById("result").value = sum 
+
+}
+
+
+
