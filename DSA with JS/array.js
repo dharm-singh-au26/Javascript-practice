300227f455c90e03dd61a32e6698f096de4184d8
Dharm Singh <dharmjat45@gmail.com>
2022-09-29 11:58:54 +0530

    insert element in array

--- DSA with JS/array.js
+++ DSA with JS/array.js
@@ -1,4 +1,33 @@
-let data = [1,5,6,8,7,2,4,6,9,0]
+/*let data = [1,5,6,8,7,2,4,6,9,0]
 for (let i = 0; i < data.length; i++) {
     console.log(`array ${i} element is ${data[i]}`) 
+}*/
+
+// insert element in array
+ /*
+let data = [60,30,3,9,10,50]
+
+let newEl = 90
+let position = 5
+
+for (let i = data.length -1; i >= 0; i--) {
+    if(i>=position){
+        data[i+1] = data[i];
+        if(i==position){
+            data[i]=newEl
+        }
+    }
+    
 }
+console.log(data)
+*/
+// insert using splice method 
+/*
+let data = [60,30,3,9,10,50]
+console.log(data)
+ data.splice(2,0,9000)
+
+ console.log(data)
+ */
+
+
